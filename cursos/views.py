e2dc5a7adc739db796a6b660671b7ffa66d755cb
thiagoalmeidadon <thiagodons@gmail.com>
2021-12-06 02:08:11 -0300

    implementando urls cursos

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
 
-# Create your views here.
+class CursoAPIView(APIView):
+    """API de cursos"""
+    def get(self, request):
+        cursos = Curso.objects.all()
+        #quando tem muitos passar o parametro many=
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+    
+class AvaliacaoAPIView(APIView):
+    """API avaliação do curso""" 
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
