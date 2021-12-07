f2088b97265a4884bc5e91138ee83f80002872a7
thiagoalmeidadon <thiagodons@gmail.com>
2021-12-07 20:45:18 -0300

    paginação

--- cursos/views.py
+++ cursos/views.py
@@ -47,8 +47,17 @@ class CursoViewSet(viewsets.ModelViewSet):
     
     @action(detail=True, methods=['get'])
     def avaliacoes(self, request, pk=None):
-        curso = self.get_object()
-        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        # paginação global do DRF não influencia no método sobreescrito
+        # é necessario implementar manualmente 
+        self.pagination_class.page_size = 1
+        avaliacoes = Avaliacao.objects.filter(curso_id=pk)
+        page = self.paginate_queryset(avaliacoes)
+        
+        if page is not None:
+            serializer = AvaliacaoSerializer(page, many=True)
+            return self.get_paginate_response(serializer.data)
+        
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
     
 """    
