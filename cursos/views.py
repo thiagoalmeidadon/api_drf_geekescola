bc6890d4bb691dfd60fd308f148ef1096e614d86
thiagoalmeidadon <thiagodons@gmail.com>
2021-12-07 16:42:31 -0300

    implementando ModelViewSet e routers

--- cursos/views.py
+++ cursos/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 from .models import Curso, Avaliacao 
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
+from rest_framework import viewsets
+from rest_framework.decorators import action  
+from rest_framework.response import Response
+
+""" API V1 """
 # ListCreateAPIView lista e cria 
 # RetrieveUpdateDestroyAPIView é implementado de outra forma e continua a ação do CRUD
 class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -16,6 +22,33 @@ class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
     
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'), pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+    
 class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+    
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+    
+
+""" APi V2 """
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
+    
+class AvaliacaoViewSet(viewsets.ModelViewSet):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
