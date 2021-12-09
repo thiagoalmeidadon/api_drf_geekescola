73167ca37374cbed08039b68002f69c03b8b2171
thiagoalmeidadon <thiagodons@gmail.com>
2021-12-08 21:52:17 -0300

    implementando classe de permissao, permissions

--- cursos/views.py
+++ cursos/views.py
@@ -8,6 +8,8 @@ from rest_framework import viewsets
 from rest_framework.decorators import action  
 from rest_framework.response import Response
 from rest_framework import mixins
+from rest_framework import permissions
+from .permissions import EhSuperUser
 
 """ API V1 """
 # ListCreateAPIView lista e cria 
@@ -42,6 +44,10 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
 """ APi V2 """
 
 class CursoViewSet(viewsets.ModelViewSet):
+    permission_classes = (
+        EhSuperUser,
+        permissions.DjangoModelPermissions,
+    )
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
     
